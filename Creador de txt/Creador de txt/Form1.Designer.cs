0dae98302b1e6a13976772bac398615a7edc037c
accusync70 <lizanderangel@gmail.com>
2017-09-17 19:38:34 -0400

    Creador
    Este form crea el archivo txt eligiendo una ruta o tomando la ruta D://
    por default.

---
+++ Creador de txt/Creador de txt/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Creador_de_txt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(305, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 156);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Location = new System.Drawing.Point(42, 192);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(234, 31);
+            this.btnSaveAs.TabIndex = 4;
+            this.btnSaveAs.Text = "Crear archivo y guardar";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(42, 131);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(234, 33);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Crear archivo TSS en ruta preterminada";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 339);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Archivo TSS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Creador de txt/Creador de txt/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Creador_de_txt
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GeneradorArchivo(string ruta)
+        {
+            using (System.IO.StreamWriter w = File.AppendText(ruta))
+            {
+      
+                w.WriteLine("E, 130-99999-1, GRUPO CORTEFIEL, 17/09/2017");
+                w.WriteLine("D, 001-00223225-4, Angel Lizander, Evangelista, Valenzuela, 19.000.00, 1.200.00");
+                w.WriteLine("S, 4, 100.000.00, 50.000.00");
+                MessageBox.Show("Archivo generado con exito");
+
+            }
+          
+        }
+
+         private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile1 = new SaveFileDialog();
+            savefile1.Filter = "Text Files (.txt)|*.txt";
+            savefile1.DefaultExt = "txt";
+            savefile1.AddExtension = true;
+            savefile1.DefaultExt = "txt";
+            savefile1.ShowDialog();
+            string saveas = savefile1.FileName;
+            GeneradorArchivo(saveas);
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string a = "D://TSS.txt";
+            GeneradorArchivo(a);
+        }
+    }
+}
