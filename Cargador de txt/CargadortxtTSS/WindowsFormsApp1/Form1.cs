67807fb2d7e854b877b974cfd33bb5ee68b51a83
accusync70 <lizanderangel@gmail.com>
2017-09-22 00:15:39 -0400

    Final
    El escritor escribe las lineas corretamente, y el lector lee
    multilineas.

--- Cargador de txt/CargadortxtTSS/WindowsFormsApp1/Form1.cs
+++ Cargador de txt/CargadortxtTSS/WindowsFormsApp1/Form1.cs
@@ -14,7 +14,6 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
 
-
         public Form1()
         {
             InitializeComponent();
@@ -22,10 +21,6 @@ namespace WindowsFormsApp1
 
         }
 
-        private void lectortxt()
-        {
-
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -36,51 +31,20 @@ namespace WindowsFormsApp1
         {
 
         }
-        private void leer(string dir, string rnc, string proceso, string fecha)
-        {
-            string[] rows = File.ReadAllLines(dir);
-            string[] encabezado = rows[0].Split(',');
-            encabezado[1] = proceso;
-            encabezado[2] = rnc;
-            encabezado[3] = fecha;
-
-
-
-
-        }
         public void button3_Click(object sender, EventArgs e)
         {
             OpenFileDialog abrir = new OpenFileDialog();
             abrir.Filter = "Text Files (.txt)|*.txt";
             abrir.ShowDialog();
 
-            string[] rows = File.ReadAllLines(abrir.FileName);
-            //Aquï empieza la lectura
-            string[] encabezado = rows[0].Split(',');
-            string[] sumario = rows[rows.GetLength(0) - 1].Split(',');
-
-            //Aquí se ejecuta el código si cumple con las exigencias
-
-            for (int i = 1; i < rows.GetLength(0) - 1; i++)
+            using (Cargador cargar = new Cargador(abrir.FileName))
             {
-                if (Convert.ToString(encabezado[0]) != "E" | rows[i].Split(',')[0] != "D" | sumario[0] != "S")
-                {
-                    MessageBox.Show("Su documento tiene un formato erróneo");
-
-                }
-
-                else
-                {
-
-                    using (Cargador cargar = new Cargador(abrir.FileName))
-                    {
-                        cargar.ShowDialog();
-                    }
-
-
-
-                }
+                cargar.ShowDialog();
             }
+
+
         }
     }
 }
+    
+
