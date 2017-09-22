2a62aa0ae8d9a32a4381489cc5f0a9e68b43a174
accusync70 <lizanderangel@gmail.com>
2017-09-21 22:45:56 -0400

    Ultimo

---
+++ Cargador de txt/CargadortxtTSS/WindowsFormsApp1/Cargador.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Cargador : Form
+    {
+        private string directorio;
+        public Cargador(string dir)
+        {
+            InitializeComponent();
+            this.directorio = dir;
+
+        }
+
+        public void Cargador_Load(object sender, EventArgs e)
+        {
+
+            string[] rows = File.ReadAllLines(this.directorio);
+            string[] encabezado = rows[0].Split(',');
+            txtproceso.Text = encabezado[1];
+            txtRNC.Text = encabezado[2];
+            txtperiodo.Text = encabezado[3];
+
+
+            //Aquí se ejecuta el código si cumple con las exigencias
+
+          
+          
+          
+            var dt = new DataTable();
+            dt.Clear();
+            dt.Columns.Add("Código de nómina", typeof(string));
+            dt.Columns.Add("Tipo de documento", typeof(string));
+            dt.Columns.Add("Documento", typeof(string));
+            dt.Columns.Add("Nombre", typeof(string));
+            dt.Columns.Add("Sexo", typeof(string));
+            dt.Columns.Add("Primer Apellido", typeof(string));
+            dt.Columns.Add("Segundo Apellido", typeof(string));
+            dt.Columns.Add("Fecha de nacimiento", typeof(DateTime));
+            dt.Columns.Add("Sueldo Neto", typeof(string));
+            dt.Columns.Add("Descuento", typeof(string));
+            dt.Columns.Add("Sueldo secundario", typeof(string));
+
+            // string a = string.Concat(encabezado[1]);
+
+                for (int i = 1; i < rows.GetLength(0) - 1; i++)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr["Código de nómina"] = rows[i].Split(',')[1];
+                    dr["Tipo de documento"] = rows[i].Split(',')[2];
+                    dr["Documento"] = rows[i].Split(',')[3];
+                    dr["Nombre"] = rows[i].Split(',')[4];
+                    dr["Sexo"] = rows[i].Split(',')[5];
+                    dr["Primer apellido"] = rows[i].Split(',')[6];
+                    dr["Segundo Apellido"] = rows[i].Split(',')[7];
+                    dr["Fecha de nacimiento"] = Convert.ToDateTime(rows[i].Split(',')[8]);
+                    dr["Sueldo neto"] = rows[i].Split(',')[9];
+                    dr["Descuento"] = rows[i].Split(',')[10];
+                    dr["Sueldo secundario"] = rows[i].Split(',')[11];
+                    dt.Rows.Add(dr);
+                }
+            
+
+            dataGridView1.DataSource = dt;
+            dataGridView1.Refresh();
+
+
+            //Tomando el resumen
+            string[] resumen = rows[rows.GetLength(0) - 1].Split(',');
+            txtdatos.Text = resumen[1];
+
+                }
+
+}
+        }
+
+       
+
--- Cargador de txt/CargadortxtTSS/WindowsFormsApp1/Form1.cs
+++ Cargador de txt/CargadortxtTSS/WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,13 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
+
         public Form1()
         {
             InitializeComponent();
+
+
         }
 
         private void lectortxt()
@@ -31,13 +36,51 @@ namespace WindowsFormsApp1
         {
 
         }
+        private void leer(string dir, string rnc, string proceso, string fecha)
+        {
+            string[] rows = File.ReadAllLines(dir);
+            string[] encabezado = rows[0].Split(',');
+            encabezado[1] = proceso;
+            encabezado[2] = rnc;
+            encabezado[3] = fecha;
 
-        private void button3_Click(object sender, EventArgs e)
+
+
+
+        }
+        public void button3_Click(object sender, EventArgs e)
         {
             OpenFileDialog abrir = new OpenFileDialog();
             abrir.Filter = "Text Files (.txt)|*.txt";
             abrir.ShowDialog();
-            string ruta = abrir.FileName;
+
+            string[] rows = File.ReadAllLines(abrir.FileName);
+            //Aquï empieza la lectura
+            string[] encabezado = rows[0].Split(',');
+            string[] sumario = rows[rows.GetLength(0) - 1].Split(',');
+
+            //Aquí se ejecuta el código si cumple con las exigencias
+
+            for (int i = 1; i < rows.GetLength(0) - 1; i++)
+            {
+                if (Convert.ToString(encabezado[0]) != "E" | rows[i].Split(',')[0] != "D" | sumario[0] != "S")
+                {
+                    MessageBox.Show("Su documento tiene un formato erróneo");
+
+                }
+
+                else
+                {
+
+                    using (Cargador cargar = new Cargador(abrir.FileName))
+                    {
+                        cargar.ShowDialog();
+                    }
+
+
+
+                }
+            }
         }
     }
 }
